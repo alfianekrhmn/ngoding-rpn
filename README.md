0d86d8c965ece6f361b81864eda061210c2f83fa
alfianekrhmn <alfian.eka481@gmail.com>
2024-05-28 14:29:57 +0700

    push ujian2

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ngoding-rpn
 don't have description
+ayo kita belajar bagaimana menjadi seorang programer yang handal
+kita disini belajar untuk menjadi seorang backend developer karena backend developer adalah cita cita saya
+mempelajari fundamental javascript adalah hal wajib bagi saya.
--- ujian week 1/ujian2.js
+++ ujian week 1/ujian2.js
@@ -32,7 +32,19 @@ return result
   console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
 
   function mengelompokkanAngka(arr) {
-    
+    let empty1 = []
+    let empty2 = []
+    let empty3 = []
+    for(let i = 0; i <= arr.length; i++){
+      if(arr[i] % 2 === 0 && arr[i] % 3){
+        empty1.push(arr[i])
+      }else if(arr[i] % 2 === 1 && arr[i] % 3){
+        empty2.push(arr[i])
+      }else if(arr[i] % 3 === 0){
+        empty3.push(arr[i])
+      }
+    }
+    return [empty1, empty2, empty3]
   }
   
   // TEST CASES
@@ -40,3 +52,39 @@ return result
   console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
   console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
   console.log(mengelompokkanAngka([])); // [ [], [], [] ]
+
+  
+function groupAnimals(animals) {
+  // menyaring animals berdasarkan hurufnya
+  animals.sort()
+
+  // membuat array kosong
+  const groupedAnimals = []
+
+  // membuat array untuk grup dan animals pertama
+  let currentGroup = [animals[0]]
+  
+  // ngeloop array animals kedua
+  for(let i = 1; i < animals.length; i++){
+    // mengecek apakah huruf pertama dari hewan saat ini cocok dengan huruf pertama kelompok saat in
+    if(animals[i][0] === currentGroup[0][0]){
+      // jika benar, maka masukan ke currentGroup
+      currentGroup.push(animals[i])
+    } else {
+      // jika tidak match, masukan ke currentGroup ke groupedAnimals dan buat grup baru
+      groupedAnimals.push(currentGroup)
+      currentGroup = [animals[i]]
+    }
+  }
+  // masukan grup terakhir ke groupedAnimals
+  groupedAnimals.push(currentGroup)
+
+  // return groupedAnimals
+  return groupedAnimals
+}
+
+// TEST CASES
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
