fa99bfd7ce976889db790f51f5e4b9b8839c3514
alfianekrhmn <alfian.eka481@gmail.com>
2024-05-29 19:00:28 +0700

    commit task 1

---
+++ ujian tanpa object/beforeObject.js
@@ -0,0 +1,67 @@
+function angkaPrima(angka) {
+  let isPrime = true
+    if(angka > 1){
+      for(let i = 2; i < angka; i++){
+        if(angka % i === 0){
+          isPrime = false
+          break
+        }
+      }
+      if(isPrime){
+        return true
+      }else{
+        return false
+      }
+    }
+  }
+  
+  // TEST CASES
+  console.log(angkaPrima(3)); // true
+  console.log(angkaPrima(7)); // true
+  console.log(angkaPrima(6)); // false
+  console.log(angkaPrima(23)); // true
+  console.log(angkaPrima(33)); // false  
+
+  function fpb(angka1, angka2) {
+    
+  }
+  
+  // TEST CASES
+  console.log(fpb(12, 16)); // 4
+  console.log(fpb(50, 40)); // 10
+  console.log(fpb(22, 99)); // 11
+  console.log(fpb(24, 36)); // 12
+  console.log(fpb(17, 23)); // 1
+
+  function cariMedian(arr) {
+    // you can only write your code here!
+  }
+  
+  // TEST CASES
+  console.log(cariMedian([1, 2, 3, 4, 5])); // 3
+  console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
+  console.log(cariMedian([3, 4, 7, 6, 10])); // 6
+  console.log(cariMedian([1, 3, 3])); // 3
+  console.log(cariMedian([7, 7, 8, 8])); // 7.5
+   
+  function cariModus(arr) {
+    // you can only write your code here!
+  }
+  
+  // TEST CASES
+  console.log(cariModus([10, 4, 5, 2, 4])); // 4
+  console.log(cariModus([5, 10, 10, 6, 5])); // 5
+  console.log(cariModus([10, 3, 1, 2, 5])); // -1
+  console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
+  console.log(cariModus([7, 7, 7, 7, 7])); // -1
+
+  function ubahHuruf(kata) {
+    // you can only write your code here!
+  }
+  
+  // TEST CASES
+  console.log(ubahHuruf('wow')); // xpx
+  console.log(ubahHuruf('developer')); // efwfmpqfs
+  console.log(ubahHuruf('javascript')); // kbwbtdsjqu
+  console.log(ubahHuruf('keren')); // lfsfo
+  console.log(ubahHuruf('semangat')); // tfnbohbu
