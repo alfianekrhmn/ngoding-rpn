1d68a0d2dd5f90390edfdffc6a5d51c6c16543f0
alfianekrhmn <alfian.eka481@gmail.com>
2024-06-10 20:03:32 +0700

    task number 2

---
+++ ujian tanpa object/9-6.js
@@ -0,0 +1,56 @@
+function changeMe(arr) {
+    
+  }
+  
+  // TEST CASES
+  changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]); // 1. Christ Evans:
+  // Christ Evans: { firstName: 'Christ',
+  //   lastName: 'Evans',
+  //   gender: 'Male',
+  //   age: 41 } 2023 - 1982 = 41 kan yak wkwk
+  // Robert Downey: { firstName: 'Robert',
+  //   lastName: 'Downey',
+  //   gender: 'Male',
+  //   age: 'Invalid Birth Year' }
+  
+  changeMe([]); // ""
+
+  function shoppingTime(memberId, money) {
+    const product = {
+        sepatuStacattu: 1500000,
+        bajuZoro: 500000,
+        bajuHnn: 250000,
+        sweatherUnikloh: 175000,
+        casingHandphone: 50000
+    }
+    if(memberId === ``){
+        return `Mohon maaf, toko X hanya berlaku untuk member saja`
+    }else if(money < 50000){
+        return `Mohon maaf, uang tidak cukup`
+    }else if(memberId == null && money == null){
+        return `Mohon maaf, toko X hanya berlaku untuk member saja`
+    }else if(memberId !== '' && money > 50000){
+        
+    }
+  }
+  
+  // TEST CASES
+  console.log(shoppingTime('1820RzKrnWn08', 2475000));
+    //{ memberId: '1820RzKrnWn08',
+    // money: 2475000,
+    // listPurchased:
+    //  [ 'Sepatu Stacattu',
+    //    'Baju Zoro',
+    //    'Baju H&N',
+    //    'Sweater Uniklooh',
+    //    'Casing Handphone' ],
+    // changeMoney: 0 }
+  console.log(shoppingTime('82Ku8Ma742', 170000));
+  //{ memberId: '82Ku8Ma742',
+  // money: 170000,
+  // listPurchased:
+  //  [ 'Casing Handphone' ],
+  // changeMoney: 120000 }
+  console.log(shoppingTime('', 2475000)); //Mohon maaf, toko X hanya berlaku untuk member saja
+  console.log(shoppingTime('234JdhweRxa53', 15000)); //Mohon maaf, uang tidak cukup
+  console.log(shoppingTime()); ////Mohon maaf, toko X hanya berlaku untuk member saja
